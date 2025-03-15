77148e18920529143a5838ee0f06ada246596a6d
DevDario <dariosilva13222@gmail.com>
2025-03-15 14:38:24 +0000

    feat: closes app when all windows are closed

--- main.js
+++ main.js
@@ -13,3 +13,9 @@ const createWindow = () =>{
 app.whenReady().then(()=>{
     createWindow()
 })
+
+app.on("window-all-closed",()=>{
+    if(process.platform !== "darwin"){
+        app.quit()
+    }
+})
